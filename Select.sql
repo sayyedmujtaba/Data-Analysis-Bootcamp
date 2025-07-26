b799ed47b3ab6f3bdf14fd11719c2321a607fbb3
Syed Mujtaba Hussain <135517051+sayyedmujtaba@users.noreply.github.com>
2025-07-26 07:43:53 +0500

    Add files via upload

---
+++ Select.sql
@@ -0,0 +1,11 @@
+SELECT * # Select all columns
+FROM parks_and_recreation.employee_demographics;
+
+# We can also select specific columns and perform operations
+SELECT first_name, age, (age * 10) + 2, gender
+FROM parks_and_recreation.employee_demographics;
+# MySQL uses PEMDAS (Parentheses, Exponents, Multiplication, Division, Addition, Subtraction) for mathematical operations
+
+# DISTINCT is used to find uniqueness in a column
+SELECT DISTINCT gender
+FROM parks_departments.employee_demographics;
