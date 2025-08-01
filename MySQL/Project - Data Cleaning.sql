5b3bc6c9413f140ee96a5f1f004c9ee82bfc81c6
Syed Mujtaba <smujtabah14709@gmail.com>
2025-08-01 18:19:59 +0500

    project - datacleaning, 27:49

--- MySQL/Project - Data Cleaning.sql
+++ MySQL/Project - Data Cleaning.sql
@@ -31,7 +31,8 @@ FROM layoffs_staging;
 with duplicate_cte as
 (
 	SELECT *,
-	ROW_NUMBER() OVER (PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+	ROW_NUMBER() OVER (
+    PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
 	FROM layoffs_staging
 )
 select *
@@ -51,15 +52,85 @@ from layoffs_staging
 where company = 'Casper'
 ;
 -- the first and third row is a duplicate
+-- to remove the extra rows lets create a new table exactly as the layoffs_stagig but with an additional column row_num
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- now as the table is created lets INSERT data
+insert into layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER (
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+-- here all the columns from layoffs_staging are mentioned
+
+-- now lets delete if any rows are apperared more than once
+delete
+from layoffs_staging2
+where row_num>1
+;
+
+-- now lets check if the there are still rows apperaed more than once
+select *
+from layoffs_staging2
+where row_num>1
+;
+-- now we can see that there are no extra rows
+
+-- stage 1 completed
+
+-- stage 2: standardization; finding and fixing issues in data
+-- it is best practice if we look at all the columns of a table
+select *
+from layoffs_staging2;
+
+-- checking for companies
+select company, trim(company)
+from layoffs_staging2;
+
+update layoffs_staging2
+set company = trim(company);
+
+-- -- checking for industries
+select distinct industry
+from layoffs_staging2
+order by 1;
+
+select *
+from layoffs_staging2
+where industry like 'Crypto%';
+
+update layoffs_staging2
+set industry = 'Crypto'
+where industry like 'Crypto%';
+
+-- checking for countries
+select distinct country
+from layoffs_staging2
+order by 1;
+
+select distinct country, trim(trailing '.' from country)
+from layoffs_staging2
+order by 1;
+
+update layoffs_staging2
+set country = trim(trailing '.' from country)
+where country like 'United States%';
+
+select *
+from layoffs_staging2
+order by country;
 
 
-
-
-
-
-
-
-
-
-
-
+-- continue from 27:49
